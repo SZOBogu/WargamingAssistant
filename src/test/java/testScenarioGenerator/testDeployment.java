2da4e525727609093b72a250898799de44be9327
Bogu <adambozek1@gmail.com>
2020-02-03 12:20:53 +0100

    Copy constructor and equals() tested for Deployment.

--- src/test/java/testScenarioGenerator/testDeployment.java
+++ src/test/java/testScenarioGenerator/testDeployment.java
@@ -6,11 +6,12 @@ import scenarioGenerator.Deployment;
 import javax.swing.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class testDeployment {
     ImageIcon img = new ImageIcon("/home/bogu/IdeaProjects/WargamingAssistant//src/main/img/T9A/deployments/1.png");
     Deployment deployment = new Deployment("Deployment", img);
-
+    Deployment deployment0 = new Deployment(deployment);
     @Test
     void getName(){
         assertEquals("Deployment", deployment.getName());
@@ -19,5 +20,18 @@ public class testDeployment {
     @Test
     void getImg(){
         assertEquals(img, deployment.getImg());
+    }
+
+    @Test
+    void testCopyConstructor(){
+        assertEquals(deployment.getName(), deployment0.getName());
+        assertEquals(deployment.getImg(), deployment0.getImg());
+    }
+
+    @Test
+    void testEquals(){
+        assertEquals(deployment, deployment0);
+        Deployment deploymentTest = new Deployment("Deployment", img);
+        assertEquals(deploymentTest, deployment);
     }
 }
