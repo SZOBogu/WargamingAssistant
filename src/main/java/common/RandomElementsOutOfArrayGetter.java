9f862c37a2b244cd0c02652868054dabc73a5be2
Bogu <adambozek1@gmail.com>
2020-02-11 22:05:05 +0100

    RandomElementsOutOfArrayGetter ready and tested.

--- src/main/java/common/RandomElementsOutOfArrayGetter.java
+++ src/main/java/common/RandomElementsOutOfArrayGetter.java
@@ -1,16 +1,63 @@
 package common;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RandomElementsOutOfArrayGetter {
-    public List<Object> randomArrayWithoutReps(List<Object> list, int elementCount){
-        return list;
+    public List<Object> randomArrayWithoutReps(List<Object> list, int howManyElements){
+        List<Object> resultList = new ArrayList<>();
+        ArrayList<Object> elementList = new ArrayList<>(list);
+        if(elementList.size() < howManyElements)
+            return resultList;
+        else{
+            for(int i = 0; i < howManyElements; i++){
+                Dice dice  = new Dice(elementList.size());
+                int roll = dice.roll() - 1;
+                resultList.add(elementList.get(roll));
+                elementList.remove(roll);
+            }
+        }
+        return resultList;
     }
 
-    public List<Object> randomArrayWithReps(List<Object> list, List<Integer> repCountList){
-        return list;
+    public List<Object> randomArrayWithReps(List<Object> list, List<Integer> repCountList, int howManyElements){
+        List<Object> resultList = new ArrayList<>(list);
+
+        if(list.size() > repCountList.size()){
+            for(int i = 0; i < list.size() - repCountList.size(); i++){
+                repCountList.add(1);
+            }
+        }
+        else if(list.size() < repCountList.size()){
+            for(int i = 0; i < repCountList.size() - list.size(); i++){
+                repCountList.remove(repCountList.size() - 1);
+            }
+        }
+
+        for(int i = 0; i < list.size(); i++){
+            for(int j = 0; j < repCountList.get(i); j++){
+                resultList.add(list.get(i));
+            }
+        }
+
+        resultList = this.randomArrayWithoutReps(resultList, howManyElements);
+        return resultList;
     }
 
-    public List<Object> randomArrayWithReps(List<Object> list, int reps){
-        return list;
+    public List<Object> randomArrayWithReps(List<Object> list, int reps, int howManyElements){
+        List<Object> resultList = new ArrayList<>();
+        for(int i = 0; i < reps; i++){
+            resultList.addAll(list);
+        }
+        resultList = this.randomArrayWithoutReps(resultList, howManyElements);
+        return resultList;
+    }
+
+    public List<Object> randomArrayWithAnyReps(List<Object> list, int howManyElements){
+        List<Object> resultList = new ArrayList<>();
+        Dice dice = new Dice(list.size());
+        for(int i = 0; i < howManyElements; i++){
+            resultList.add(list.get(dice.roll() - 1));
+        }
+        return resultList;
     }
 }
--- src/test/java/testCommon/testRandomElementsOutOfArrayGetter.java
+++ src/test/java/testCommon/testRandomElementsOutOfArrayGetter.java
@@ -3,28 +3,72 @@ package testCommon;
 import common.RandomElementsOutOfArrayGetter;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class testRandomElementsOutOfArrayGetter {
     RandomElementsOutOfArrayGetter getter = new RandomElementsOutOfArrayGetter();
-    List<Object> dummyList = Arrays.asList("1", "2", "3");
+    List<Object> dummyList = Arrays.asList(1,2,3);
 
     @Test
     void testRandomArrayWithoutReps() {
-
+        List<Object> resultList = getter.randomArrayWithoutReps(dummyList, 2);
+        assertEquals(resultList.size(), 2);
+        assertTrue((Integer)resultList.get(0) > 0 && (Integer)resultList.get(0) < 4);
+        assertTrue((Integer)resultList.get(1) > 0 && (Integer)resultList.get(1) < 4);
+        assertNotSame(resultList.get(1), resultList.get(0));
     }
 
     @Test
     void testRandomArrayWithRepsArray() {
-
+        List<Integer> repList = Arrays.asList(5,2,0);
+        List<Object> resultList = getter.randomArrayWithReps(dummyList, repList, 3);
+        assertEquals(resultList.size(), 3);
+        assertTrue((Integer)resultList.get(0) > 0 && (Integer)resultList.get(0) < 4);
+        assertTrue((Integer)resultList.get(1) > 0 && (Integer)resultList.get(1) < 4);
+        assertTrue((Integer)resultList.get(2) > 0 && (Integer)resultList.get(2) < 4);
+        assertNotSame(resultList.get(0), dummyList.get(2));
+        assertNotSame(resultList.get(1), dummyList.get(2));
+        assertNotSame(resultList.get(2), dummyList.get(2));
+        int reps = 0;
+        for(int i = 0 ; i < resultList.size(); i++){
+            if((Integer)resultList.get(i) == 2) {
+                reps++;
+            }
+        }
+        if(reps > 2){
+            fail();
+        }
     }
 
     @Test
     void testRandomArrayWithRepsInt() {
+        List<Object> resultList = getter.randomArrayWithReps(dummyList, 2, 3);
+        assertEquals(resultList.size(), 3);
+        assertTrue((Integer)resultList.get(0) > 0 && (Integer)resultList.get(0) < 4);
+        assertTrue((Integer)resultList.get(1) > 0 && (Integer)resultList.get(1) < 4);
+        assertTrue((Integer)resultList.get(2) > 0 && (Integer)resultList.get(2) < 4);
 
+        for(int i = 0 ; i < dummyList.size(); i++){
+            int reps = 0;
+            for(int j = 0; j < resultList.size(); j++){
+                if(resultList.get(j) == dummyList.get(i))
+                    reps++;
+            }
+            if(reps > 2)
+                fail();
+            else
+                reps = 0;
+        }
     }
 
-
+    @Test
+    void testRandomArrayWithAnyReps() {
+        List<Object> resultList = getter.randomArrayWithAnyReps(dummyList, 2);
+        assertEquals(resultList.size(), 2);
+        assertTrue((Integer)resultList.get(0) > 0 && (Integer)resultList.get(0) < 4);
+        assertTrue((Integer)resultList.get(1) > 0 && (Integer)resultList.get(1) < 4);
+    }
 }
